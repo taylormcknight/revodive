791eefa14d14c38dc44d3067b04593b3ff5c5d9e
Taylor McKnight <mcknight@MacBook-Pro.fios-router.home>
2021-01-10 18:49:29 -0500

    updates terms of service page

---
+++ pages/terms.js
@@ -0,0 +1,20 @@
+// components/Base.js
+import BasicLayout from '../components/page-layouts/BasicLayout'
+import PageSection from '../components/PageSection'
+
+function TOS() {
+  return (
+    <BasicLayout
+        HeroImage={false}
+        hero="/images/about.jpg"
+        title="RevoDive Terms of Service"
+        mainclass="main"
+    >
+    <PageSection>
+        
+    </PageSection>
+    </BasicLayout>
+  )
+}
+
+export default TOS
