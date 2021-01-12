fddf1abee7930ee74b1df7a06acba7090bc22473
Taylor McKnight <mcknight@MacBook-Pro.fios-router.home>
2021-01-11 21:55:33 -0500

    adds suggestion component

---
+++ components/Suggestion.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const Suggestion = () => (
+	<Link href="/suggestions">
+	<a className="button secondary suggestion">
+	Suggest an update
+		</a>
+	</Link>
+)
+
+export default Suggestion;
